8232662e56caf55b9a938fc87b37114864c25f6f
Condrea Flavius <condreaflavius2003@gmail.com>
2023-04-14 15:10:13 +0200

    tema_nr_3

---
+++ ProblemeLAB4_CondreaFlavius1711C.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  13 2:15:30 2023
+
+@author: student
+"""
+
+'''
+Pe un stadion există trei categorii de locuri.
+Locurile din clasa A costă 20 dolari, locurile din clasa B costă 15 dolari, iar locurile din clasa C costă 10 dolari.
+Scrieți un program modularizat care  solicităutilizatorului  introducerea  numărului  de  bilete  vândute  din  fiecare  clasă
+apoi  afișează  suma veniturilor generate din vânzarea biletelor.Utilizati funcții pentru a rezolva problema.
+'''
+import math
+
+num1 = input("nr1 ->> ")
+num2 = input("nr2 ->> ")
+suma = sum(num1,num2)
+    

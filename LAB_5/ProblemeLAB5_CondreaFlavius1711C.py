09e3c73bb1db5dca3d7482ecbf41d1ba0f67019b
Condrea Flavius <condreaflavius2003@gmail.com>
2023-04-17 17:00:08 +0200

    ---

---
+++ LAB_5/ProblemeLAB5_CondreaFlavius1711C.py
@@ -0,0 +1,11 @@
+import sys
+import time
+import random
+
+'''
+Scrieți un program care citește și salvează într-un fișier numere.txt o serie de numere întregi. 
+Utilizați fișierul create pentru a scrieun program care calculează media arithmetică a tuturor  numerelor stocate în fișier.
+'''
+
+def problema_1():
+    
